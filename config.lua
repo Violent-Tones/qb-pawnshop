19a7753bbe600dc4efd2565644b8e1069ce33c15
Bryan Gallardo <bryanjgallardo@gmail.com>
2024-10-07 14:18:53 -0700

    Pawn item prices increased.

--- config.lua
+++ config.lua
@@ -24,35 +24,35 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 Config.PawnItems = {
     [1] = {
         item = 'goldchain',
-        price = math.random(50,100)
+        price = math.random(200,800)
     },
     [2] = {
         item = 'diamond_ring',
-        price = math.random(50,100)
+        price = math.random(300,1200)
     },
     [3] = {
         item = 'rolex',
-        price = math.random(50,100)
+        price = math.random(500,2000)
     },
     [4] = {
         item = 'tenkgoldchain',
-        price = math.random(50,100)
+        price = math.random(300,1000)
     },
     [5] = {
         item = 'tablet',
-        price = math.random(50,100)
+        price = math.random(50,200)
     },
     [6] = {
         item = 'iphone',
-        price = math.random(50,100)
+        price = math.random(100,400)
     },
     [7] = {
         item = 'samsungphone',
-        price = math.random(50,100)
+        price = math.random(80,320)
     },
     [8] = {
         item = 'laptop',
-        price = math.random(50,100)
+        price = math.random(150,600)
     }
 }
 
